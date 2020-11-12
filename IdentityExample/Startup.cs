c4267cad8656ff652ef9c53eac57c79e3361a619
Ranga Fernando <ranga.fernando@gmail.com>
2020-11-13 07:04:53 +1100

    Add in memory database configurations

--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -8,16 +8,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using IdentityExample.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityExample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<AppDbContext>(config =>
+            {
+                config.UseInMemoryDatabase("InMemory");
+            });
+            
             var authSchema = "Cookie.Auth";
+
             services.AddAuthentication()
                 .AddCookie(authSchema, o =>
                 {
@@ -28,8 +35,7 @@ namespace IdentityExample
             services.AddControllersWithViews();
 
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
